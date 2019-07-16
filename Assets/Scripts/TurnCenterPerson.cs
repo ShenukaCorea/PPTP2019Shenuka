11b42d42d73a38c95bc3422ae698ddfc0349a7be
Shenuka Corea <smc828@ad.nyu.edu>
2019-07-16 14:47:10 +0400

    new scene experiment with completed script

---
+++ Assets/Scripts/TurnCenterPerson.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnCenterPerson : MonoBehaviour
+{
+	public Sprite middlepersonr;
+	public Sprite middlepersonl;
+	public CameraController camCon;
+	private float leftpersondist;
+	private float rightpersondist;
+	GameObject leftperson, rightperson, middleperson;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftperson = GameObject.Find("leftperson");
+        rightperson = GameObject.Find("rightperson");
+        middleperson = GameObject.Find("middleperson");
+        camCon = GameObject.Find("Main Camera").GetComponent<CameraController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftpersondist = leftperson.GetComponent<ProximityToCenter>().GetCenterDistance();
+        //Debug.Log("LEFT PERSON is " + leftpersondist + " pixels from the center");
+        rightpersondist = rightperson.GetComponent<ProximityToCenter>().GetCenterDistance();
+        //Debug.Log("RIGHT PERSON is " + rightpersondist + " pixels from the center");
+        if( camCon.canObserve()){
+	        if ( leftpersondist>rightpersondist){
+	        	Debug.Log("RIGHT PERSON is closer to center");
+	        	this.GetComponent<SpriteRenderer>().sprite=middlepersonr;
+	        }
+	        else{
+	        	Debug.Log("LEFT PERSON is closer to center");
+	        	this.GetComponent<SpriteRenderer>().sprite=middlepersonl;
+	        }
+        }
+    }
+}
---
+++ Assets/Scripts/powerswing.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerswing : MonoBehaviour
+{
+	public Sprite authoritybig;
+	public Sprite authoritysmall;
+	public Sprite subjectbig;
+	public Sprite subjectsmall;
+	public CameraController camCon;
+	private float authoritydist;
+	private float subjectdist;
+	GameObject authority, subject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        authority = GameObject.Find("authority");
+        subject = GameObject.Find("subject");
+        camCon = GameObject.Find("Main Camera").GetComponent<CameraController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        authoritydist = authority.GetComponent<ProximityToCenter>().GetCenterDistance();
+        Debug.Log("AUTHORITY is " + authoritydist + " pixels from the center");
+        subjectdist = subject.GetComponent<ProximityToCenter>().GetCenterDistance();
+        Debug.Log("SUBJECT is " + subjectdist + " pixels from the center");
+
+        if( camCon.canObserve()){
+	        if ( authoritydist<subjectdist){
+	        	Debug.Log("AUTHORITY is closer to center");
+	        	authority.GetComponent<SpriteRenderer>().sprite=authoritybig;
+	        	subject.GetComponent<SpriteRenderer>().sprite=subjectsmall;
+	        }
+	        else{
+	        	Debug.Log("SUBJECT is closer to center");
+	        	authority.GetComponent<SpriteRenderer>().sprite=authoritysmall;
+	        	subject.GetComponent<SpriteRenderer>().sprite=subjectbig;
+	        }
+        }
+    }
+}
